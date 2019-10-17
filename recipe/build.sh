1630fa22afd4736d51f616bb4f6bd4807680a2e1
benhuff <benhuff10@gmail.com>
2019-10-17 16:14:16 -0400

    ostype in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,5 +6,15 @@ mkdir -vp ${NLTK_DATA}
 # Download from the source of the package, its on the $SRC_DIR
 
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
-find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
-find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+
+# Check the platform to determine software to use for unzip
+
+if [ "$OSTYPE" = "msys" ]; then
+    find ${NLTK_DATA}/ -name "*.zip" -execdir 7za e -qq -o '{}' ';' -delete
+    find ${NLTK_DATA}/ -name "*.gz" -exec 7za e '{}' ';'
+
+else
+    find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
+    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+
+fi
