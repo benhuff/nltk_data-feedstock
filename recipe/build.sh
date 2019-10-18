23aed4efdea7ff4750752663dce35591a03aa176
benhuff <benhuff10@gmail.com>
2019-10-18 12:49:55 -0400

    7za formatting

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-7za --help
-
 NLTK_DATA=${PREFIX}/lib/nltk_data
 mkdir -vp ${NLTK_DATA}
 
 # Download from the source of the package, its on the $SRC_DIR
+
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 
-find ${NLTK_DATA}/ -name "*.zip" -execdir 7za e -qq -o '{}' ';' -delete
-find ${NLTK_DATA}/ -name "*.gz" -exec 7za e '{}' ';'
+if [[ "$OSTYPE" = "mysys" ]]; then
+
+    find ${NLTK_DATA}/ -name "*.zip" -execdir 7za x -r
+    find ${NLTK_DATA}/ -name "*.gz" -exec 7za x
+
+else
+
+    find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
+    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+
+fi
