4eca1bac8995826e2024e1426fa77d81b18a53ae
benhuff <benhuff10@gmail.com>
2019-10-18 14:52:47 -0400

    bld.bat testing

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,3 @@
-set "PATH=%PREFIX%\lib\nltk_data;%PATH%"
-mkdir %PATH%
-move %SRC_DIR%\packages %PATH%
-7za l -r %PATH%
+mkdir %PREFIX%\lib\nltk_data
+move %SRC_DIR%\packages %PREFIX%\lib\nltk_data
+7za l -r %PREFIX%\lib\nltk_data\packages
