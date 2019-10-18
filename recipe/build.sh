d6f49f4b4f18685e964596d7f0b6220a4c5c1ea1
benhuff <benhuff10@gmail.com>
2019-10-18 11:15:02 -0400

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" = "msys" ]]; then
-    NLTK_DATA=${PREFIX}/lib/nltk_data
-    mkdir -vp ${NLTK_DATA}
+7za --help
 
-    # Download from the source of the package, its on the $SRC_DIR
+NLTK_DATA=${PREFIX}/lib/nltk_data
+mkdir -vp ${NLTK_DATA}
 
-    mv ${SRC_DIR}/packages/* ${NLTK_DATA}
+# Download from the source of the package, its on the $SRC_DIR
+mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 
-    7za --help
-    find ${NLTK_DATA}/ -name "*.zip" -execdir 7za e -qq -o '{}' ';' -delete
-    find ${NLTK_DATA}/ -name "*.gz" -exec 7za e '{}' ';'
-
-else
-    NLTK_DATA=${PREFIX}/lib/nltk_data
-    mkdir -vp ${NLTK_DATA}
-
-    # Download from the source of the package, its on the $SRC_DIR
-
-    mv ${SRC_DIR}/packages/* ${NLTK_DATA}
-    find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
-    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
-
-fi
+find ${NLTK_DATA}/ -name "*.zip" -execdir 7za e -qq -o '{}' ';' -delete
+find ${NLTK_DATA}/ -name "*.gz" -exec 7za e '{}' ';'
