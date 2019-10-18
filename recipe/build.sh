9dac8dbb0b6f738c3352d26ec97c30877911e282
benhuff <benhuff10@gmail.com>
2019-10-18 13:01:46 -0400

    ostype = linux*

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,14 +7,14 @@ mkdir -vp ${NLTK_DATA}
 
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 
-if [[ "$OSTYPE" = "mysys" ]]; then
-
-    find ${NLTK_DATA}/ -name "*.zip" -execdir 7za x -r
-    find ${NLTK_DATA}/ -name "*.gz" -exec 7za x
-
-else
+if [[ "$OSTYPE" = linux* ]]; then
 
     find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
     find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+
+else
+
+    find ${NLTK_DATA}/ -name "*.zip" -execdir 7za x -r
+    find ${NLTK_DATA}/ -name "*.gz" -exec 7za x
 
 fi
