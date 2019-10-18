fb2e171eeba900d068a6aa7010f6c94f167df1fe
benhuff <benhuff10@gmail.com>
2019-10-18 13:57:17 -0400

    bld.bat testing

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 7za --help
 
 mkdir %PREFIX%\lib\nltk_data
-%SRC_DIR%
-copy %SRC_DIR% %PREFIX%\lib\nltk_data
+echo %PREFIX%\lib\nltk_data
+echo %SRC_DIR%
 
-7za x -r %PREFIX%\lib\nltk_data
+7za l -r %SRC_DIR%
