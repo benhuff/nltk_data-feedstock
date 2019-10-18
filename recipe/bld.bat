f07743ef33771df55e63d939c583ebc15e04021c
benhuff <benhuff10@gmail.com>
2019-10-18 15:01:53 -0400

    bld.bat testing

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
 mkdir %PREFIX%\lib\nltk_data
 move %SRC_DIR%\packages %PREFIX%\lib\nltk_data
-7za l -r %PREFIX%\lib\nltk_data\packages
+FORFILES /
+FORFILES /S /M 7za l -r %PREFIX%\lib\nltk_data\packages
