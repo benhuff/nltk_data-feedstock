89ccc91c4c92b5fe28fef2fa672a86bd51e2679d
benhuff <benhuff10@gmail.com>
2019-10-18 10:11:52 -0400

    testing

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-NLTK_DATA=${PREFIX}/lib/nltk_data
-mkdir -vp ${NLTK_DATA}
-
-# Download from the source of the package, its on the $SRC_DIR
-
-mv ${SRC_DIR}/packages/* ${NLTK_DATA}
-
-# Check the platform to determine software to use for unzip
-
 if [[ "$OSTYPE" = "msys" ]]; then
+    NLTK_DATA=${PREFIX}/lib/nltk_data
+    mkdir -vp ${NLTK_DATA}
+
+    # Download from the source of the package, its on the $SRC_DIR
+
+    mv ${SRC_DIR}/packages/* ${NLTK_DATA}
+
     7za --help
     find ${NLTK_DATA}/ -name "*.zip" -execdir 7za e -qq -o '{}' ';' -delete
     find ${NLTK_DATA}/ -name "*.gz" -exec 7za e '{}' ';'
 
 else
+    NLTK_DATA=${PREFIX}/lib/nltk_data
+    mkdir -vp ${NLTK_DATA}
+
+    # Download from the source of the package, its on the $SRC_DIR
+
+    mv ${SRC_DIR}/packages/* ${NLTK_DATA}
     find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
     find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
 
