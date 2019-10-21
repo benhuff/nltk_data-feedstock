532001426e3332c468395416f41d68f22b4cf38a
benhuff <benhuff10@gmail.com>
2019-10-21 11:09:43 -0400

    ostype detect

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,13 +6,13 @@ mkdir -vp ${NLTK_DATA}
 # Download from the source of the package, its on the $SRC_DIR
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 
-if [ "$OSTYPE" = linux ]; then
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "Linux"
     bash -c "echo $OSTYPE"
     find ${NLTK_DATA}/ -name '*.zip' -execdir unzip -qq -o '{}' ';'
     find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
 
-elif [ "$OSTYPE" = darwin ]; then
+elif [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Mac"
     bash -c "echo $OSTYPE"
     find ${NLTK_DATA}/ -name '*.zip' -execdir unzip -qq -o '{}' ';'
