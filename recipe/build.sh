d153164ff4f962eb71ada26c02fe41a0b11ad704
benhuff <benhuff10@gmail.com>
2019-10-21 08:50:43 -0400

    build.sh testing

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+bash -c "echo $OSTYPE"
+
 NLTK_DATA=${PREFIX}/lib/nltk_data
 mkdir -vp ${NLTK_DATA}
 
 # Download from the source of the package, its on the $SRC_DIR
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 
-if [[ "$OSTYPE" = cygwin ]]; then
+if [[ "$OSTYPE" != darwin* or linux* ]]; then
     find ${NLTK_DATA}/ -name '*.zip' ! -name '*propbank*.zip' -execdir unzip -qq -o '{}' ';'
     find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
 else
