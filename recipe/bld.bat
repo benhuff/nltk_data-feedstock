36ac42e4f7555ddb8c0ce930cf07b9d8297a1d53
benhuff <benhuff10@gmail.com>
2019-10-21 13:24:33 -0400

    loop

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,4 @@
 mkdir %PREFIX%\lib\nltk_data
 move %SRC_DIR%\packages %PREFIX%\lib\nltk_data
 
-for /F %%I IN ('%PREFIX%\lib\nltk_data /b /s *.zip') DO (
-    7za x -o"%%~dpI" "%%I"
-)
+7za x %PREFIX%\lib\nltk_data -air!*.zip -r -aoa
