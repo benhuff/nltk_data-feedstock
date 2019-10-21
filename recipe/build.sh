780128923db4ef6b1dd1bd6ea95d54261fbffbcc
benhuff <benhuff10@gmail.com>
2019-10-21 10:58:07 -0400

    ostype detect

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,16 +7,19 @@ mkdir -vp ${NLTK_DATA}
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 
 if [ "$OSTYPE" = linux ]; then
+    echo "Linux"
     bash -c "echo $OSTYPE"
     find ${NLTK_DATA}/ -name '*.zip' -execdir unzip -qq -o '{}' ';'
     find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
 
 elif [ "$OSTYPE" = darwin ]; then
+    echo "Mac"
     bash -c "echo $OSTYPE"
     find ${NLTK_DATA}/ -name '*.zip' -execdir unzip -qq -o '{}' ';'
     find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
 
 else
+    bash -c "Other"
     bash -c "echo $OSTYPE"
     find ${NLTK_DATA}/ -name '*.zip' ! -name '*propbank*.zip' -execdir unzip -qq -o '{}' ';'
     find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
