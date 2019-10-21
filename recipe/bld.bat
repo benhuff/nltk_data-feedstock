12790ab3ae8495215f17677fcfac4f6dd1d845ce
benhuff <benhuff10@gmail.com>
2019-10-21 08:33:14 -0400

    build.sh testing

--- recipe/bld.bat
+++
@@ -1,4 +0,0 @@
-mkdir %PREFIX%\lib\nltk_data
-cd %PREFIX%\lib\nltk_data
-move %SRC_DIR%\packages %PREFIX%\lib\nltk_data
-7za x -r
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,12 @@ NLTK_DATA=${PREFIX}/lib/nltk_data
 mkdir -vp ${NLTK_DATA}
 
 # Download from the source of the package, its on the $SRC_DIR
-
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 
-find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
-find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+if [[ "$OSTYPE" = cygwin ]]; then
+    find ${NLTK_DATA}/ -name '*.zip' ! -name '*propbank*.zip' -execdir unzip -qq -o '{}' ';'
+    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+else
+    find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
+    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+fi
