0b55b3482a554e738bf0cd4b4860f857c769c391
benhuff <benhuff10@gmail.com>
2019-10-21 11:51:48 -0400

    bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 mkdir %PREFIX%\lib\nltk_data
 move %SRC_DIR%\packages %PREFIX%\lib\nltk_data
 
-find %PREFIX%\lib\nltk_data -name "*.zip" -execdir 7za x -aoa '{}' ';'
-find %PREFIX%\lib\nltk_data -name "*.gz" -execdir 7za x -tgzip -aoa '{}' ';'
+7za x %PREFIX%\lib\nltk_data -r -aoa
+7za x %PREFIX%\lib\nltk_data -tgzip -aoa
