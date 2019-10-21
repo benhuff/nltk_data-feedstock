12113f365b5555b1b4305bb074bd5a492d7861c7
benhuff <benhuff10@gmail.com>
2019-10-21 10:32:52 -0400

    ostype detect

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-bash -c "echo $OSTYPE"
-
 NLTK_DATA=${PREFIX}/lib/nltk_data
 mkdir -vp ${NLTK_DATA}
 
 # Download from the source of the package, its on the $SRC_DIR
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 
-find ${NLTK_DATA}/ -name '*.zip' ! -name '*propbank*.zip' -execdir unzip -qq -o '{}' ';'
-find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+if [ "$OSTYPE" = linux ]; then
+    bash -c "echo $OSTYPE"
+    find ${NLTK_DATA}/ -name '*.zip' -execdir unzip -qq -o '{}' ';'
+    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+
+elif [ "$OSTYPE" = darwin ]; then
+    bash -c "echo $OSTYPE"
+    find ${NLTK_DATA}/ -name '*.zip' -execdir unzip -qq -o '{}' ';'
+    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+
+else
+    bash -c "echo $OSTYPE"
+    find ${NLTK_DATA}/ -name '*.zip' ! -name '*propbank*.zip' -execdir unzip -qq -o '{}' ';'
+    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+fi
