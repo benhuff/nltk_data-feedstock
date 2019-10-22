1b29cd07682ad2cdf03fdcee2c0628b18ca70049
benhuff <benhuff10@gmail.com>
2019-10-22 19:02:02 -0400

    bld.bat | forfiles cmd /c 7za x -tzip -r -y @file

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,4 +4,4 @@ mkdir "%NLTK_DATA%"
 move %SRC_DIR%\packages %NLTK_DATA%
 cd %NLTK_DATA%
 
-forfiles /s /m *.zip /c "cmd /c 7za e -tzip -y @file"
+forfiles /s /m *.zip /c "cmd /c 7za x -tzip -r -y @file"
