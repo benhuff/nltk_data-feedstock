4038b6eaebf50072a6626f5798a9b32083ff3a1e
benhuff <benhuff10@gmail.com>
2019-10-22 17:18:57 -0400

    bld.bat | fix error in 7za

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,7 @@
 set NLTK_DATA=%LIBRARY_PREFIX%\lib\nltk_data
 mkdir "%NLTK_DATA%"
 
-echo "Printing out LIBRARY_PREFIX\lib..."
-dir %LIBRARY_PREFIX%\lib
-
-echo "Printing out SRC_DIR\packages..."
-dir %SRC_DIR%\packages
-
 move %SRC_DIR%\packages %NLTK_DATA%
 cd %NLTK_DATA%
 
-echo "Printing out NLTK_DATA after SRC_DIR\packages\* has been moved..."
-dir %NLTK_DATA%
-
-7za l %NLTK_DATA%\packages *.zip -xr!*.xml
+for %f in (*.zip) do 7za x -r
