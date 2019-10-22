198e19191b6de85c364eecfc6e70cbfa7ccb6d44
benhuff <benhuff10@gmail.com>
2019-10-22 16:56:08 -0400

    bld.bat | fix error in move

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ dir %LIBRARY_PREFIX%\lib
 echo "Printing out SRC_DIR\packages..."
 dir %SRC_DIR%\packages
 
-move %SRC_DIR%\packages\* %NLTK_DATA%
+move %SRC_DIR%\packages %NLTK_DATA%
 cd %NLTK_DATA%
 
 echo "Printing out NLTK_DATA after SRC_DIR\packages\* has been moved..."
