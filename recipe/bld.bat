7e85eecc0193edd348d9241fdb1069b2fcf530ef
benhuff <benhuff10@gmail.com>
2019-10-22 16:04:55 -0400

    bld.bat | replace library_lib with prefix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,8 @@ dir %SRC_DIR%
 dir %SRC_DIR%\packages
 
 move "%SRC_DIR%\packages\*" "%PREFIX%\nltk_data"
-cd %LIBRARY_LIB%\nltk_data
+cd %PREFIX%\nltk_data
 
-dir %LIBRARY_LIB%\nltk_data
+dir %PREFIX%\nltk_data
 
 7za l %PREFIX%\nltk_data *.zip -x!*.xml
