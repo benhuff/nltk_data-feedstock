d57de393bd0941e4c94e977643678fcc2b5684c8
benhuff <benhuff10@gmail.com>
2019-10-22 12:12:36 -0400

    MNT: Re-rendered with conda-build 3.18.9, conda-smithy 3.6.0, and conda-forge-pinning 2019.10.11

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,5 +6,10 @@ mkdir -vp ${NLTK_DATA}
 # Download from the source of the package, its on the $SRC_DIR
 
 mv ${SRC_DIR}/packages/* ${NLTK_DATA}
-find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
-find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+
+if [[ "$OSTYPE" == "msys"* ]]; then
+    find ${NLTK_DATA}/ -name "*.zip" -execdir 7za x -ao '{}' ';' -delete
+else
+    find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
+    find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
+fi
