b04edda1d5f43129e0f09f97dd19b3ed40e97744
benhuff <benhuff10@gmail.com>
2019-10-23 11:57:48 -0400

    try xcopy without remove and without \* in .bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,7 @@
 set NLTK_DATA=%LIBRARY_PREFIX%\lib\nltk_data
 mkdir "%NLTK_DATA%"
 
-xcopy %SRC_DIR%\packages\* %NLTK_DATA%
-rmdir /S %SRC_DIR%\packages
+xcopy %SRC_DIR%\packages %NLTK_DATA%
 cd %NLTK_DATA%
 
 forfiles /s /m *.zip /c "cmd /c 7za x -tzip -r -y @file"
