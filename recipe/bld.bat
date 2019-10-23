3d1df84685b65671b85b2f988fcfb2a6248dc380
benhuff <benhuff10@gmail.com>
2019-10-23 12:08:54 -0400

    try xcopy /s dirS dirD

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,9 @@
 set NLTK_DATA=%LIBRARY_PREFIX%\lib\nltk_data
 mkdir "%NLTK_DATA%"
 
-xcopy %SRC_DIR%\packages %NLTK_DATA%
+xcopy /s %SRC_DIR%\packages %NLTK_DATA%
 cd %NLTK_DATA%
 
 forfiles /s /m *.zip /c "cmd /c 7za x -tzip -r -y @file"
 
 dir %NLTK_DATA%
-dir %NLTK_DATA%\packages
