d0a255287a7bb80ff3d56fe5e4c36827d8ecf93c
benhuff <benhuff10@gmail.com>
2019-10-23 09:48:47 -0400

    root directory same unix/win

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ mkdir -vp ${NLTK_DATA}
 
 # Download from the source of the package, its on the $SRC_DIR
 
-mv ${SRC_DIR}/packages/* ${NLTK_DATA}
+mv ${SRC_DIR}/packages ${NLTK_DATA}
 
 find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
 find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
